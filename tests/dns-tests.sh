3e0d98bb32003cac730969bd7176eb8a3a5d317f
William Desportes <williamdes@wdes.fr>
2025-03-21 11:40:48 +0100

    Fix and improve tests

--- tests/dns-tests.sh
+++ tests/dns-tests.sh
@@ -7,15 +7,24 @@ apk add --no-cache --update bind-tools 1>/dev/null
 
 EXIT_CODE=0
 
+# Arguments
+# - The name to query
+# - The exit code (default: 1)
+# - Some more options (default: none)
+#
 testQuery () {
-    _RESULT="$(dig "$1" +noall +answer +short "@${DNS_SERVER}")"
+    _RESULT="$(dig ${3:-} "$1" +noall +answer +short "@${DNS_SERVER}")"
     if [ -n "$_RESULT" ]; then
-        echo "SUCCESS for $1"
+        echo "SUCCESS for $1 ($_RESULT)"
         return;
     fi
     # Set exit code from second arg or use 1
     EXIT_CODE="${2:-1}"
-    echo "FAILURE for $1"
+    if [ "${2:-1}" = "0" ]; then
+        echo "SUCCESS (successfully failed) for $1"
+    else
+        echo "FAILURE for $1"
+    fi
 }
 
 testServer() {
@@ -31,8 +40,14 @@ testServer() {
     # A query that only the server can answer
     testQuery 'test-domain.custom'
     testQuery 'dns-server.custom'
+    testQuery '127.0.0.11' 0 '-x'
+    testQuery '8.8.8.8' 0 '-x'
+    testQuery '1.1.1.1' 0 '-x'
+
+    DNS_SERVER="127.0.0.11"
     # Using docker server
-    testQuery 'dns-server' 1 127.0.0.11
+    testQuery "${1}"
+    DNS_SERVER="${1}"
 
     echo 'Ended'
 }
